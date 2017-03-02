129963257b5ba7f881ae76b2bcee93526c22b962
Rubén <rubenzc@me.com>
2017-03-02 17:22:13 +0100

    Updated styles and added Legend

--- main.js
+++ main.js
@@ -10,9 +10,10 @@ $().ready(function(){
 var shellForm = $("<form id='shellForm'></form>");
 var divForm = $("<div id='divForm'></div>")
 
-divForm.append('<label class="control-label" for="inputAlt">Alt<input type="text" value="" id="inputAlt" placeholder="Insert Alt"></label>');
-divForm.append('<label class="control-label" for="inputTitle">Title<input type="text" id="inputTitle" value="" placeholder="Insert Title"></label>');
-divForm.append('<label class="control-label" for="inputCat">Categorie<input type="text" id="inputCat" value="" placeholder="Insert Cetegorie"></label>');
+divForm.append('<label class="control-legend" for="legendTitle">FORM FIELDS<legend value="" id="legendTitle"></label>');
+divForm.append('<label class="control-label" for="inputAlt">Alt<input type="text" value="" id="inputAlt" placeholder=" Insert Alt"></label>');
+divForm.append('<label class="control-label" for="inputTitle">Title<input type="text" id="inputTitle" value="" placeholder=" Insert Title"></label>');
+divForm.append('<label class="control-label" for="inputCat">Categorie<input type="text" id="inputCat" value="" placeholder=" Insert Cetegorie"></label>');
 divForm.append('<label class="control-label" for="buttonForm"><input type="button" id="buttonForm" value="SUBMIT"></label>');
 
 $(shellForm).append(divForm);
@@ -31,11 +32,32 @@ $("#shellForm").css({
 });
 
 $("#divForm").css({
+    'border': '2px solid #00a3ec',
+    'padding': '3%',
     'margin': '15% auto 0',
     'width': '200px'
 });
 
+$("#legendTitle .control-label").css({
+    'color': '#fff',
+    'font-size': '16px',
+    'font-weight': 'bold',
+    'margin-bottom': '20px',
+    'text-align': 'center',  
+    'width': '200px'
+});
+
+$(".control-title").css ({
+    'color': '#fff',
+    'font-size': '16px',
+    'font-weight': 'bold',
+    'margin-bottom': '20px',
+    'text-align': 'center',  
+    'width': '200px'
+});
+
 $("#inputAlt").css({
+    'border': '2px solid #00a3ec',
     'border-width': '0 !important',
     'color': '#000000',
     'height': '30px',
@@ -44,6 +66,7 @@ $("#inputAlt").css({
 });
 
 $("#inputTitle").css({
+    'border': '2px solid #00a3ec',
     'border-width': '0 !important',
     'color': '#000000',
     'height': '30px',
@@ -52,6 +75,7 @@ $("#inputTitle").css({
 });
 
 $("#inputCat").css ({
+    'border': '2px solid #00a3ec',
     'border-width': '0 !important',
     'color': '#000000',
     'height': '30px',
@@ -60,12 +84,12 @@ $("#inputCat").css ({
 });
 
 $("#buttonForm").css ({
-    'border': '1px solid !important',
-    'border-radius': '30px',
-    'background': '#00cc66',
+    'border': '0',
+    'background': '#00a3ec',
     'color': '#fff',
     'display': 'block',
-    'font-size': '12px',
+    'font-size': '14px',
+    'font-weight': 'bold',
     'height': '35px',
     'text-align': 'center',
     'text-transform': 'uppercase',
@@ -75,5 +99,11 @@ $("#buttonForm").css ({
 $(".control-label").css ({
     'display': 'inline-block',
     'margin-bottom': '5px',
-    'font-weight': '700'
+    'font-weight': '700',
+});
+
+$(".control-legend").css ({
+    'display': 'inline-block',
+    'margin-bottom': '20px',
+    'font-weight': '700',
 });
