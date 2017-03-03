24be84270cc083b633157bbc29e63f21204853f9
Rubén <rubenzc@me.com>
2017-03-03 10:32:00 +0100

    Added Field validation and submit new values

--- main.js
+++ main.js
@@ -1,28 +1,22 @@
-//////////////////*FUNCTIONS*///////////////
-$().ready(function(){
-    $("form").submit(function(){
-        alert('Evento click capturado!!');
-    });
-});
-
 //////////////////*FORM*////////////////////
 
-var shellForm = $("<form id='shellForm' action=''></form>");
-var divForm = $("<div id='divForm'></div>");
+var shellForm = jQuery("<form id='shellForm' action=''></form>");
+var divForm = jQuery("<div id='divForm'></div>");
 
+divForm.append('<label class="control-close" for="closeButton">X<span value="" id="closeButton"></label>')
 divForm.append('<label class="control-legend" for="legendTitle">FORM FIELDS<legend value="" id="legendTitle"></label>');
 divForm.append('<label class="control-label" for="inputAlt">Alt<input type="text" value="" id="inputAlt" placeholder=" Insert Alt"></label>');
 divForm.append('<label class="control-label" for="inputTitle">Title<input type="text" id="inputTitle" value="" placeholder=" Insert Title"></label>');
 divForm.append('<label class="control-label" for="inputCat">Categorie<input type="text" id="inputCat" value="" placeholder=" Insert Cetegorie"></label>');
 divForm.append('<label class="control-label" for="buttonForm"><input type="submit" id="buttonForm" value="SUBMIT"></label>');
 
-$(shellForm).append(divForm);
-$('body').append(shellForm);
+jQuery(shellForm).append(divForm);
+jQuery('body').append(shellForm);
 
 //////////////////*STYLES*//////////////////
 
-$("#shellForm").css({
-    'background': 'rgba(96, 125, 139, 0.5)',
+jQuery("#shellForm").css({
+    'background': 'rgba(0, 0, 0, 0.78)',
     'position': 'fixed',
     'top': '0',
     'bottom': '0',
@@ -31,14 +25,14 @@ $("#shellForm").css({
 
 });
 
-$("#divForm").css({
+jQuery("#divForm").css({
     'border': '2px solid #00a3ec',
     'padding': '3%',
     'margin': '15% auto 0',
     'width': '200px'
 });
 
-$("#legendTitle .control-label").css({
+jQuery("#legendTitle .control-label").css({
     'color': '#fff',
     'font-size': '16px',
     'font-weight': 'bold',
@@ -47,7 +41,7 @@ $("#legendTitle .control-label").css({
     'width': '200px'
 });
 
-$(".control-title").css ({
+jQuery(".control-title").css ({
     'color': '#fff',
     'font-size': '16px',
     'font-weight': 'bold',
@@ -56,7 +50,7 @@ $(".control-title").css ({
     'width': '200px'
 });
 
-$("#inputAlt").css({
+jQuery("#inputAlt").css({
     'border': '2px solid #00a3ec',
     'border-width': '0 !important',
     'color': '#000000',
@@ -65,7 +59,7 @@ $("#inputAlt").css({
     'width': '200px'
 });
 
-$("#inputTitle").css({
+jQuery("#inputTitle").css({
     'border': '2px solid #00a3ec',
     'border-width': '0 !important',
     'color': '#000000',
@@ -74,7 +68,7 @@ $("#inputTitle").css({
     'width': '200px'
 });
 
-$("#inputCat").css ({
+jQuery("#inputCat").css ({
     'border': '2px solid #00a3ec',
     'border-width': '0 !important',
     'color': '#000000',
@@ -83,7 +77,7 @@ $("#inputCat").css ({
     'width': '200px'
 });
 
-$("#buttonForm").css ({
+jQuery("#buttonForm").css ({
     'border': '0',
     'background': '#00a3ec',
     'color': '#fff',
@@ -96,14 +90,43 @@ $("#buttonForm").css ({
     'width': '204px'
 });
 
-$(".control-label").css ({
+jQuery(".control-label").css ({
     'display': 'inline-block',
     'margin-bottom': '5px',
     'font-weight': '700',
+    'color': '#fff'
 });
 
-$(".control-legend").css ({
+jQuery(".control-legend").css ({
+    'color': '#fff',
     'display': 'inline-block',
     'margin-bottom': '20px',
     'font-weight': '700',
+});
+
+//////////////////*FUNCTIONS*///////////////
+
+
+jQuery().ready(function(){
+    
+    jQuery("form").submit(function(){
+
+        var inputAlt = jQuery('#inputAlt').val();
+        var inputTitle = jQuery('#inputTitle').val();
+        var inputCat = jQuery('#inputCat').val();
+        /*var formOk = false;*/
+
+        //Validate inputAlt field
+        if (inputAlt == ""){
+
+            alert("El campo Alt debe tener un descripción");
+            return false;
+
+        }else{
+
+            jQuery('#the-list .column input').val(inputAlt) && jQuery('#the-list .column textarea').val(inputTitle) && jQuery('#the-list .column-6 textarea').val(inputCat);
+            return false;
+        }
+
+    });
 });
